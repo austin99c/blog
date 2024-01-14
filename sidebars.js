04bc63f1233f12a7c08b1efb51ade6759bb07fda
shanyu <hbh3737@163.com>
2024-01-14 19:39:11 +0800

    feat(init)

---
+++ sidebars.js
@@ -0,0 +1,126 @@
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+ */
+
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  // By default, Docusaurus generates a sidebar from the docs folder structure
+  tutorialSidebar: [
+
+    // "browser",
+    "HTML",
+    "helloMarkdown",
+    // {
+    //   type: 'category',
+    //   label: 'JavaScript',
+    //   link: {
+    //     type: 'doc',
+    //     id: 'javascript/basic',
+    //   },
+    //   items: [
+    //     'javascript/数组',
+    //     'javascript/对象',
+    //     'javascript/函数',
+    //     'javascript/Class',
+    //     'javascript/Promise',
+    //     'javascript/Generator',
+    //     {
+    //       type: 'doc',
+    //       id: 'javascript/Set-Map',
+    //       label: 'Set And Map'
+    //     },
+    //     'javascript/AJAX',
+    //     // 'javascript/Fetch',
+    //     'javascript/BOM',
+    //     'javascript/DOM',
+    //     'javascript/正则',
+    //     'javascript/Date',
+    //     'javascript/Proxy',
+    //     // 'javascript/Reflect',
+    //     // {
+    //     //   type: 'doc',
+    //     //   id:  'javascript/Set-Map',
+    //     //   label: 'Set/Map'
+    //     // },
+    //     'javascript/装饰器',
+    //     // 'javascript/Graphql',
+    //     // 'javascript/BOM',
+    //     // 'javascript/DOM',
+    //     // 'javascript/事件',
+    //     'javascript/杂项'
+    //   ],
+    // },
+    // // "javascript",
+    // {
+    //   type: 'category',
+    //   label: 'Typescript',
+    //   items: [
+    //     {
+    //       type: 'doc',
+    //       id: 'typescript/config',
+    //       label: 'tsconfig.json'
+    //     },
+    //     {
+    //       type: 'doc',
+    //       id: 'typescript/declaration',
+    //       label: 'd.ts'
+    //     },
+    //     {
+    //       type: 'doc',
+    //       id: 'typescript/basic',
+    //       label: '基础'
+    //     },
+    //     {
+    //       type: 'doc',
+    //       id: 'typescript/类型操作.mdx',
+    //     },
+    //     {
+    //       type: 'doc',
+    //       id: 'typescript/类型守卫',
+    //     },
+    //     {
+    //       type: 'doc',
+    //       id: 'typescript/类型断言',
+    //     },
+    //     {
+    //       type: 'doc',
+    //       id: 'typescript/声明合并',
+    //     },
+    //     {
+    //       type: 'doc',
+    //       id: 'typescript/逆变与协变',
+    //     },
+    //     {
+    //       type: 'doc',
+    //       id: 'typescript/工具类型',
+    //     },
+    //     {
+    //       type: 'doc',
+    //       id: 'typescript/版本更新日志.mdx',
+    //     },
+    //   ],
+    // },
+  ],
+
+  // But you can create a sidebar manually
+  /*
+  tutorialSidebar: [
+    {
+      type: 'category',
+      label: 'Tutorial',
+      items: ['hello'],
+    },
+  ],
+   */
+};
+
+module.exports = sidebars;
